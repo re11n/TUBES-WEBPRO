781a4c2e51ba220da15b03422c85bb748e1c348f
degenerate <dawskutel@gmail.com>
2022-11-07 02:02:45 +0700

    Add login and react router dom

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,9 +3,46 @@ import React, { Component } from 'react'
 export class Login extends Component {
   render() {
     return (
-    <div>
-        <h2> Login disini</h2>
-    </div>
+      <form>
+      <h3>Login</h3>
+
+      <div className="mb-3">
+        <label>Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Enter email"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Enter password"
+        />
+      </div>
+
+      <div className="mb-3">
+        <div className="custom-control custom-checkbox">
+          <input
+            type="checkbox"
+            className="custom-control-input"
+            id="customCheck1"
+          />
+          <label className="custom-control-label" htmlFor="customCheck1">
+          Remember me
+          </label>
+        </div>
+      </div>
+
+      <div className="d-grid">
+        <button type="submit" className="btn btn-primary">
+          Login 
+        </button>
+      </div>
+    </form>
     )
   }
 }
