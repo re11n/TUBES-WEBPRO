7568a59e7a177da2999d17144b2ff4fe986e6340
degenerate <dawskutel@gmail.com>
2022-11-12 18:25:17 +0700

    progres tubes

--- src/Components/Jadwal.js
+++ src/Components/Jadwal.js
@@ -1,44 +1,14 @@
-import Table from 'react-bootstrap/Table';
-import React, { Component } from 'react'
 
-function BasicExample() {
+import Formulir from './Formulir';
+
+function Jadwal() {
     return (
-    
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-          <tr>
-          
-          </tr>
-        </tbody>
-        <img src='https://i.ibb.co/j5BDRbS/hehe.jpg' className='h-100'></img>   
-      </Table>
+   <>
+   <h1>Beli Tiket Bus Online Untuk Bepergian ke Rute Favorit Anda</h1>
+    <Formulir/>
+    </>
     );
 
   }
   
-  export default BasicExample;
+  export default Jadwal;
