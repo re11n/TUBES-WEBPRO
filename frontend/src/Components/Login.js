cd0096a58a47b49757e44ddf412afa4a460079ec
re11n <ryansiahaan@student.telkomuniversity.ac.id>
2022-11-13 16:26:43 +0700

    banyak dah

---
+++ frontend/src/Components/Login.js
@@ -0,0 +1,223 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Form from "./Forms";
+import Axios from "axios";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [remember, setRemember] = useState(false);
+  const [validate, setValidate] = useState({});
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [loginStatus, setLoginStatus] = useState("");
+
+  Axios.defaults.withCredentials = true;
+  const logingab = () => {
+    Axios.post("http://localhost:3001/login", {
+      email: email, 
+      password: password
+    }).then((response) =>{
+
+      if (response.data.message){
+        setLoginStatus(response.data.message)
+      } else {
+        setLoginStatus(response.data[0].nama)
+      }
+    });
+    alert("Successfully Login");
+  }
+
+
+  const validateLogin = () => {
+    let isValid = true;
+
+    let validator = Form.validator({
+      email: {
+        value: email,
+        isRequired: true,
+        isEmail: true,
+      },
+      password: {
+        value: password,
+        isRequired: true,
+        minLength: 6,
+      },
+    });
+
+    if (validator !== null) {
+      setValidate({
+        validate: validator.errors,
+      });
+
+      isValid = false;
+    }
+    return isValid;
+  };
+
+  const authenticate = (e) => {
+    e.preventDefault();
+
+    const validate = validateLogin();
+
+    if (validate) {
+      setValidate({});
+      logingab();
+      setEmail("");
+      setPassword("");
+      
+    }
+  };
+
+  const togglePassword = (e) => {
+    if (showPassword) {
+      setShowPassword(false);
+    } else {
+      setShowPassword(true);
+    }
+  };
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/login").then((response) => {
+      if (response.data.LoggedIn == true){
+        setLoginStatus(response.data.user[0].nama)
+      }
+    })
+  }, []);
+
+  return (
+    
+    <div className="row g-0 auth-wrapper">
+      <div className="col-12 col-md-5 col-lg-6 h-100 auth-background-col">
+      <img src="https://i.ibb.co/gRVpp5c/chuttersnap-O-bo-SKI5-ZFY-unsplash.jpg" className="h-100 col-12" alt="img-login"></img>
+        <div className="auth-background-holder">
+        </div>
+        <div className="auth-background-mask">
+        </div>
+      </div>
+
+      <div className="col-12 col-md-7 col-lg-6 auth-main-col text-center">
+        <div className="d-flex flex-column align-content-end">
+          <div className="auth-body mx-auto square border rounded">
+            <h1> {loginStatus}</h1>
+            <p className="auth-txt">Login to your account</p>
+            <div className="auth-form-container text-start">
+              <form
+                className="auth-form"
+                method="POST"
+                onSubmit={authenticate}
+                autoComplete={"off"}
+              >
+                <div className="email mb-3">
+                  <input
+                    type="email"
+                    className={`form-control ${
+                      validate.validate && validate.validate.email
+                        ? "is-invalid "
+                        : ""
+                    }`}
+                    id="email"
+                    name="email"
+                    value={email}
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+
+                  <div
+                    className={`invalid-feedback text-start ${
+                      validate.validate && validate.validate.email
+                        ? "d-block"
+                        : "d-none"
+                    }`}
+                  >
+                    {validate.validate && validate.validate.email
+                      ? validate.validate.email[0]
+                      : ""}
+                  </div>
+                </div>
+
+                <div className="password mb-3">
+                  <div className="input-group">
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      className={`form-control ${
+                        validate.validate && validate.validate.password
+                          ? "is-invalid "
+                          : ""
+                      }`}
+                      name="password"
+                      id="password"
+                      value={password}
+                      placeholder="Password"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    <button
+                      type="button"
+                      className="btn btn-outline-primary btn-sm"
+                      onClick={(e) => togglePassword(e)}
+                    >
+                      <i
+                        className={
+                          showPassword ? "far fa-eye" : "far fa-eye-slash"
+                        }
+                      ></i>{" "}
+                    </button>
+
+                    <div
+                      className={`invalid-feedback text-start ${
+                        validate.validate && validate.validate.password
+                          ? "d-block"
+                          : "d-none"
+                      }`}
+                    >
+                      {validate.validate && validate.validate.password
+                        ? validate.validate.password[0]
+                        : ""}
+                    </div>
+                  </div>
+
+                  <div className="extra mt-3 row justify-content-between">
+                    <div className="col-6">
+                      <div className="form-check">
+                        <input
+                          className="form-check-input"
+                          type="checkbox"
+                          id="remember"
+                          checked={remember}
+                          onChange={(e) => setRemember(e.currentTarget.checked)}
+                        />
+                        <label className="form-check-label" htmlFor="remember">
+                          Remember me
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="text-center">
+                  <button
+                    type="submit"
+                    className="btn btn-primary w-100 theme-btn mx-auto"
+                  >
+                    Log In
+                  </button>
+                </div>
+              </form>
+
+              <hr />
+              <div className="auth-option text-center pt-2">
+                No Account?{" "}
+                <Link className="text-link" to="/register">
+                  Sign up{" "}
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+  );
+};
+
+export default Login;
