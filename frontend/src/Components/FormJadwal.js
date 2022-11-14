c97c252df3d928d82c152b2aa84d2f8a127385db
Fiffah <itsmefiffah@gmail.com>
2022-11-14 13:16:13 +0700

    perubahan jadwal

--- frontend/src/Components/FormJadwal.js
+++ frontend/src/Components/FormJadwal.js
@@ -1,9 +1,17 @@
-
+import { Nav } from 'react-bootstrap';
+import { 
+    // eslint-disable-next-line
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 import React,{useState} from "react";
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import Tiket from './Tiket';
 
 function Formulir() {
     const [setDate] = useState();
@@ -22,8 +30,22 @@ function Formulir() {
         </Col>
       </Row>
     </Form>
-    <Button variant="primary" >Lihat Jadwal</Button>
+    <Nav.Link as={Link} to="/Tiket"><Button variant="primary" >Lihat Jadwal</Button></Nav.Link>
+    <div>
+        <Router>
+      <Switch>
+        <Route path="/Tiket">
+          <Tiket/>
+        </Route>
+        <Route path="/Tiket">
+          <Tiket/>
+        </Route>
+      </Switch>
+      </Router>
+    </div>
+   
         </>
+    
     );
 }
 
