e5112fa159848f9aa68a59c479bff3128d210e2f
fanramdhan17 <olioaglio55@gmail.com>
2022-11-14 14:35:18 +0700

    tabel ugl;y bastard

--- frontend/src/Components/FormJadwal.js
+++ frontend/src/Components/FormJadwal.js
@@ -12,11 +12,17 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Tiket from './Tiket';
+import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
+
+
+
 
 function Formulir() {
     const [setDate] = useState();
     return (  
         <>
+        <Card>
            <Form>
       <Row>
         <Col>
@@ -31,6 +37,39 @@ function Formulir() {
       </Row>
     </Form>
     <Nav.Link as={Link} to="/Tiket"><Button variant="primary" >Lihat Jadwal</Button></Nav.Link>
+    </Card>
+
+    <Card>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+    </Card>
     <div>
         <Router>
       <Switch>
